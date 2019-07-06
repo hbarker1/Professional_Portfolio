18f87d423325ee60baf80adaa9ac045cad591b67
hannah barker <hannah.m.barker@gmail.com>
2019-07-06 17:19:35 -0400

    first commit

---
+++ components/front-page/front-page.js
@@ -0,0 +1,17 @@
+function FrontPageController(recipeService) {
+    const ctrl = this;
+}
+
+  angular
+    .module('Portfolio')
+    .component('FrontPage', {
+      template: `
+      
+    `, // or use templateUrl
+      controller: FrontPageController,
+      // bindings: {
+      //   me: '<',
+      //   onDelete: '&',
+      //   onUpdate: '&'
+      // }
+    });
---
+++ components/projects/projects.js
@@ -0,0 +1,17 @@
+function ProjectsController(recipeService) {
+    const ctrl = this;
+}
+
+  angular
+    .module('Portfolio')
+    .component('Projects', {
+      template: `
+      
+    `, // or use templateUrl
+      controller: ProjectsController,
+      // bindings: {
+      //   me: '<',
+      //   onDelete: '&',
+      //   onUpdate: '&'
+      // }
+    });
